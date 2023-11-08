66ec33beca2abd31cf808f0b372732537d0ebe20
alamin <alamin.dolar1@gmail.com>
2023-11-08 18:02:57 +0600

    req and res handle

--- index.js
+++ index.js
@@ -2,7 +2,21 @@
 
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("Hello Node js");
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.write(
+      JSON.stringify({ name: "Al-amin Dolar", age: 20, profession: "Engineer" })
+    );
+    res.end();
+  } else if (req.url == "/contact") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This Is a Contact page</p>");
+    res.end();
+  } else if (req.url == "/about") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This Is a Contact page</p>");
+    res.end();
+  }
 });
 
 const PORT = 5000;
